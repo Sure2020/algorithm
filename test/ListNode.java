05be9ba3cc94c76342419495c9a5db79ad3a01bc
wangfengshuo <wangfengshuo2012@163.com>
2023-10-16 00:01:57 +0800

    单向链表的初始化和遍历

---
+++ test/ListNode.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : LinkedNode
+ * Date Created : 2023-10-15
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2023-10-15       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: w15021
+ * @create: 2023-10-15
+ **/
+public class ListNode {
+  int val;
+  ListNode next;
+  ListNode() {}
+  ListNode(int val) { this.val = val; }
+  ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
