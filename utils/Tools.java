9bdc6eb4a2412f0968fc3b3a24fecd263d65ce25
wangfengshuo <wangfengshuo2012@163.com>
2023-10-18 11:10:50 +0800

    add Tools.java for create and print linkedlist

---
+++ utils/Tools.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : Tools
+ * Date Created : 2023-10-18
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2023-10-18       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+import java.util.ArrayList;
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: w15021
+ * @create: 2023-10-18
+ **/
+public class Tools {
+    public static ListNode listNodeCreater(int[] sourceList) {
+        if (sourceList == null || sourceList.length == 0) {
+            return null;
+        }
+        /*if (sourceList.length == 0){
+            return new ListNode(0);
+        }*/
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        for (int i : sourceList) {
+            head.next = new ListNode(i);
+            head = head.next;
+        }
+        return dummy.next;
+    }
+
+    public static void listNodePrinter(ListNode listNode) {
+        ListNode head = listNode;
+        while (head != null) {
+            System.out.println(head.val);
+            head = head.next;
+        }
+    }
+}
