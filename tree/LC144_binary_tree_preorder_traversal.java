d134ad973a311a4b97a66f89bfbcf75292c12213
wangfengshuo <wangfengshuo2012@163.com>
2023-12-16 11:14:55 +0800

    LC144_binary_tree_preorder_traversal

---
+++ tree/LC144_binary_tree_preorder_traversal.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : LC144_binary_tree_preorder_traversal
+ * Date Created : 2023-12-14
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2023-12-14       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: w15021
+ * @create: 2023-12-14
+ **/
+public class LC144_binary_tree_preorder_traversal {
+    static List<Integer> result = new ArrayList<>();
+    public static void main(String[] args) {
+        System.out.println("case1");
+        TreeNode tempNode = new TreeNode();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        tempNode = root.right;
+        tempNode.left = new TreeNode(15);
+        tempNode.right = new TreeNode(7);
+        preorderTraversal(root);
+
+        System.out.println("case2");
+        TreeNode tempNode2 = new TreeNode();
+        TreeNode root2 = new TreeNode(1);
+        root2.right = new TreeNode(2);
+        result.clear();
+        preorderTraversal(root2);
+    }
+    public static List<Integer> preorderTraversal(TreeNode root) {
+
+        traverse(root);
+        Tools.listPriter(result);
+        return result;
+
+    }
+    public static void traverse(TreeNode root) {
+        if (root == null){
+            return;
+        }
+        result.add(root.val);
+        traverse(root.left);
+        traverse(root.right);
+    }
+}
