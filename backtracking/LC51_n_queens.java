7cf2cfdfcb16a7c5f15245797359d43932d9c760
wangfengshuo <wangfengshuo2012@163.com>
2023-12-28 16:43:57 +0800

    LC51_n_queens to do

---
+++ backtracking/LC51_n_queens.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : NERV
+ * Module Name  : LC51_n_queens
+ * Date Created : 2023-12-28
+ * Creator      : w15021
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2023-12-28       w15021     xxx
+ * --------------------------------------------------------------------
+ */
+
+import java.util.List;
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: w15021
+ * @create: 2023-12-28
+ **/
+public class LC51_n_queens {
+    public static void main(String[] args) {
+
+    }
+    // todo
+    public List<List<String>> solveNQueens(int n) {
+        return null;
+
+    }
+}
