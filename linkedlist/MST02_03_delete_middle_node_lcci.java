fe5318732e7a066de9a6b20d3d7e892835fee370
Sure <wangfengshuo2012@163.com>
2024-01-09 10:29:08 +0800

    MST02_03_delete_middle_node_lcci

---
+++ linkedlist/MST02_03_delete_middle_node_lcci.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : XXX
+ * Module Name  : MST_02_03_delete_middle_node_lcci
+ * Date Created : 2024-01-09
+ * Creator      : Admin
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2024-01-09       Admin     xxx
+ * --------------------------------------------------------------------
+ */
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: Admin
+ * @create: 2024-01-09
+ **/
+public class MST02_03_delete_middle_node_lcci {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+
+    }
+}
