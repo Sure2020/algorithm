fedf7624545261205614988a1bafbca30b146a68
Sure <wangfengshuo2012@163.com>
2024-01-31 23:27:29 +0800

    LC100_same_tree

---
+++ tree/LC100_same_tree.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : XXX
+ * Module Name  : LC100_same_tree
+ * Date Created : 2024-01-31
+ * Creator      : Admin
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2024-01-31       Admin     xxx
+ * --------------------------------------------------------------------
+ */
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: Admin
+ * @create: 2024-01-31
+ **/
+public class LC100_same_tree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null&&q==null){
+            return true;
+        }
+        if(p==null||q==null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+}
