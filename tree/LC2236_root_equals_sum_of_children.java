9f90dfa4d15bd3bf0312c76a420b6c4f53cac4d5
Sure <wangfengshuo2012@163.com>
2024-02-02 11:13:51 +0800

    LC2236_root_equals_sum_of_children

---
+++ tree/LC2236_root_equals_sum_of_children.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : XXX
+ * Module Name  : LC2236_root_equals_sum_of_children
+ * Date Created : 2024-02-02
+ * Creator      : Admin
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2024-02-02       Admin     xxx
+ * --------------------------------------------------------------------
+ */
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: Admin
+ * @create: 2024-02-02
+ **/
+public class LC2236_root_equals_sum_of_children {
+    public boolean checkTree(TreeNode root) {
+        return root.val == (root.left.val + root.right.val);
+    }
+}
