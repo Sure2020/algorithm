fc9170f5439047437fe19ecef96a0ee004b6afb9
Sure <wangfengshuo2012@163.com>
2024-02-02 10:51:11 +0800

    LC292_nim_game

---
+++ mind/LC292_nim_game.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : XXX
+ * Module Name  : LC292_nim_game
+ * Date Created : 2024-02-02
+ * Creator      : Admin
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2024-02-02       Admin     xxx
+ * --------------------------------------------------------------------
+ */
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: Admin
+ * @create: 2024-02-02
+ **/
+public class LC292_nim_game {
+    public boolean canWinNim(int n) {
+        return n%4!=0;
+    }
+}
