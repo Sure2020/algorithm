b523e49bb0f10bcc01c92cb49f1b723080e5e8d2
Sure <wangfengshuo2012@163.com>
2024-02-03 23:08:31 +0800

    bfs

---
+++ tree/BFS.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : XXX
+ * Module Name  : BFS
+ * Date Created : 2024-02-03
+ * Creator      : Admin
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2024-02-03       Admin     xxx
+ * --------------------------------------------------------------------
+ */
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: Admin
+ * @create: 2024-02-03
+ **/
+public class BFS {
+    public static void main(String[] args) {
+        TreeNode root = Tools.treeCreater(new int[]{1,2,3,-1,5,-1,4});
+        Tools.printTree(root);
+        bfs(root);//12354
+    }
+    public static void bfs(TreeNode root){
+        if(root==null){
+            return;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            TreeNode tempNode = q.poll();
+            System.out.println(tempNode.val);
+            if(tempNode.left==null&&tempNode.right==null){
+                System.out.println("leaf node");
+            }
+            if(tempNode.left!=null){
+                q.offer(tempNode.left);
+            }
+            if(tempNode.right!=null){
+                q.offer(tempNode.right);
+            }
+        }
+    }
+}
