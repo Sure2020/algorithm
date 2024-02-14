e4ebe19255d7a761521f33cbcc204bbc66b1b77b
Sure <wangfengshuo2012@163.com>
2024-02-15 02:02:04 +0800

    LC236_lowest_common_ancestor_of_a_binary_tree,Solution20240215

--- tree/LC236_lowest_common_ancestor_of_a_binary_tree.java
+++ tree/LC236_lowest_common_ancestor_of_a_binary_tree.java
@@ -44,4 +44,23 @@ public class LC236_lowest_common_ancestor_of_a_binary_tree {
             return left != null ?left:right;
         }
     }
+    class Solution20240215 {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            return dfs(root, p.val,q.val);
+        }
+        public TreeNode dfs(TreeNode root, int val1, int val2){
+            if(root==null){
+                return null;
+            }
+            if(root.val==val1||root.val==val2){
+                return root;
+            }
+            TreeNode left = dfs(root.left,val1,val2);
+            TreeNode right = dfs(root.right,val1,val2);
+            if(left!=null&&right!=null){
+                return root;
+            }
+            return left!=null?left:right;
+        }
+    }
 }
