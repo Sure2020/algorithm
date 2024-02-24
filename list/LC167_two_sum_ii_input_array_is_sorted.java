bb4a9d5b09c31a75ae3cdbfa9caa60f43482cf12
Sure <wangfengshuo2012@163.com>
2024-02-24 23:29:49 +0800

    LC167_two_sum_ii_input_array_is_sorted，Solution20240224

--- list/LC167_two_sum_ii_input_array_is_sorted.java
+++ list/LC167_two_sum_ii_input_array_is_sorted.java
@@ -51,4 +51,21 @@ public class LC167_two_sum_ii_input_array_is_sorted {
         }
         return new int[]{};
     }
+    class Solution20240224 {
+        public int[] twoSum(int[] numbers, int target) {
+            int left=0,right=numbers.length-1;
+            int sum = 0;
+            for(int i =0;i<numbers.length;i++){
+                sum=numbers[left]+numbers[right];
+                if(sum==target){
+                    return new int[]{left+1, right+1};
+                }else if(sum<target){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
+            return new int[]{-1,-1};
+        }
+    }
 }
