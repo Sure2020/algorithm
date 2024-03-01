0670ace1e1542ff7c924cf0fe51ee2ff9a6c913e
Sure <wangfengshuo2012@163.com>
2024-03-01 23:41:07 +0800

    LC88_merge_sorted_array,这题第二次做还是没想起来思路，记住这个从后往前填充的思路吧！ 好在看了思路，自己一遍过了

--- list/LC88_merge_sorted_array.java
+++ list/LC88_merge_sorted_array.java
@@ -19,6 +19,8 @@
 /**
  * @program: PACKAGE_NAME
  * @description: xxx
+ * 这题第二次做还是没想起来思路，记住这个从后往前填充的思路吧！
+ * 好在看了思路，自己一遍过了
  * @author: Admin
  * @create: 2024-01-11
  **/
@@ -54,6 +56,28 @@ public class LC88_merge_sorted_array {
             nums1[index3]=nums2[index2];
             index2--;
             index3--;
+        }
+    }
+
+    class Solution20240301 {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int index1=m-1,index2=n-1,p=m+n-1;
+            while(p>=0&&index1>=0&&index2>=0){
+                if(nums1[index1]>=nums2[index2]){
+                    nums1[p]=nums1[index1];
+                    index1--;
+                }else{
+                    nums1[p]=nums2[index2];
+                    index2--;
+
+                }
+                p--;
+            }
+            while(index2>=0){
+                nums1[p]=nums2[index2];
+                index2--;
+                p--;
+            }
         }
     }
 }
