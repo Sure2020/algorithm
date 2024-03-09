c5f1256e8e7df45aad946acbd51c6433e5ba9743
Sure <wangfengshuo2012@163.com>
2024-03-09 10:24:49 +0800

    LC92_reverse_linked_list_ii,Solution20240309,此次的感受是，不知道base case时如何处理，就带入只有一个节点的链表来感受一下即可明了。仍然不能独立写出，还要再练练

--- linkedlist/LC92_reverse_linked_list_ii.java
+++ linkedlist/LC92_reverse_linked_list_ii.java
@@ -23,7 +23,7 @@
  * @author: Admin
  * @create: 2024-03-03
  **/
-public class LC_reverse_linked_list_ii {
+public class LC92_reverse_linked_list_ii {
     class SolutionSelfWrong {
         ListNode successor = null;
         public ListNode reverseBetween(ListNode head, int left, int right) {
@@ -85,6 +85,38 @@ public class LC_reverse_linked_list_ii {
             ListNode last = reverseN(head.next, n-1);
             head.next.next = head;
             head.next = successor;
+            return last;
+        }
+    }
+
+    class Solution20240309 {
+        //此次的感受是，不知道base case时如何处理，就带入只有一个节点的链表来感受一下即可明了。
+        //仍然不能独立写出，还要再练练。
+        public ListNode reverseBetween(ListNode head, int left, int right) {
+            //这里，先前进到第left个节点，那么后面的节点就可以按翻转前n个节点来处理
+            if(left==1){
+                //此时相当于翻转前n个节点，开始按reverseN的方法处理本节点以及后面的节点即可
+                return reverseN(head, right);
+            }
+            //left之前的节点，还是原样拼起来，所以用head.next来承接
+            head.next= reverseBetween(head.next, left-1, right-1);
+            return head;
+        }
+
+        //翻转前n个节点，先找到第n+1个节点并记下来，记为successor，并返回当前节点，也就是到此节点为止，后面的节点不管
+        //再按翻转全部链表的方法，最后head.next不再指向null，而是指向第n+1个节点即可
+        ListNode successor = null;
+        public ListNode reverseN (ListNode head, int n){
+            if(head==null||head.next==null){
+                return head;
+            }
+            if(n==1){
+                successor=head.next;
+                return head;
+            }
+            ListNode last = reverseN(head.next, n-1);
+            head.next.next=head;
+            head.next=successor;
             return last;
         }
     }
