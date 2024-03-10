40a4e5a66a97520ef3e7c14e024b7850c457f3eb
Sure <wangfengshuo2012@163.com>
2024-03-10 23:14:10 +0800

    LC94_binary_tree_inorder_traversal

---
+++ tree/LC94_binary_tree_inorder_traversal.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2018, New H3C Technologies Co., Ltd All rights reserved
+ * <http://www.h3c.com/>
+ * --------------------------------------------------------------------
+ * Product      : XXX
+ * Module Name  : LC94_binary_tree_inorder_traversal
+ * Date Created : 2024-03-10
+ * Creator      : Admin
+ * Description  : xxx
+ *
+ * --------------------------------------------------------------------
+ * Modification History
+ * DATE             NAME                DESCRIPTION
+ * --------------------------------------------------------------------
+ * 2024-03-10       Admin     xxx
+ * --------------------------------------------------------------------
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program: PACKAGE_NAME
+ * @description: xxx
+ * @author: Admin
+ * @create: 2024-03-10
+ **/
+public class LC94_binary_tree_inorder_traversal {
+    class Solution {
+        List<Integer> result = new ArrayList<>();
+        public List<Integer> inorderTraversal(TreeNode root) {
+            traverse(root);
+            return result;
+        }
+        public void traverse(TreeNode root){
+            if(root == null){
+                return;
+            }
+            traverse(root.left);
+            result.add(root.val);
+            traverse(root.right);
+        }
+    }
+
+    class Solution20240310 {
+        List<Integer> result = new ArrayList<>();
+        public List<Integer> inorderTraversal(TreeNode root) {
+            if(root==null){
+                return result;
+            }
+            inorderTraversal(root.left);
+            result.add(root.val);
+            inorderTraversal(root.right);
+            return result;
+        }
+    }
+}
