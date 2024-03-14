e324cc874026481c79ffecbed8d9c159fb32a2ec
Sure <wangfengshuo2012@163.com>
2024-03-14 08:13:55 +0800

    LC236_lowest_common_ancestor_of_a_binary_tree,Solution20240313,这题，第三次再遇到，只有个模糊的印象，知道有3种情况，其实只要记住，就是去子树中找p或者q节点，写出find函数后面就水到渠成了

--- tree/LC236_lowest_common_ancestor_of_a_binary_tree.java
+++ tree/LC236_lowest_common_ancestor_of_a_binary_tree.java
@@ -63,4 +63,23 @@ public class LC236_lowest_common_ancestor_of_a_binary_tree {
             return left!=null?left:right;
         }
     }
+    //这题，第三次再遇到，只有个模糊的印象，知道有3种情况，其实只要记住，就是去子树中找p或者q节点，写出find函数后面就水到渠成了
+    class Solution20240313 {
+        //分3种情况，1。两节点都在当前节点的子树中；2.两节点都不在；3.一个在，一个不在
+        //这题，第三次再遇到，只有个模糊的印象，知道有3种情况，其实只要记住，就是去子树中找p或者q节点，写出find函数后面就水到渠成了
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if(root==null){
+                return null;
+            }
+            if(root.val==p.val || root.val==q.val){
+                return root;
+            }
+            TreeNode left = lowestCommonAncestor(root.left, p,q);
+            TreeNode right = lowestCommonAncestor(root.right, p,q);
+            if(left!=null && right !=null){
+                return root;
+            }
+            return left!=null ? left:right;
+        }
+    }
 }
