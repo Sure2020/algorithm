0a3621a3f813f35e6df367dfcc1932a4fec940a1
Sure <wangfengshuo2012@163.com>
2024-03-29 07:27:38 +0800

    LC704_binary_search,Solution20240329

--- binarysearch/LC704_binary_search.java
+++ binarysearch/LC704_binary_search.java
@@ -78,4 +78,24 @@ public class LC704_binary_search {
 
         }
     }
+    class Solution20240329 {
+        public int search(int[] nums, int target) {
+            if(nums==null){
+                return -1;
+            }
+            int left=0,right=nums.length-1;
+
+            while(left<=right){
+                int mid = left+(right-left)/2;
+                if(nums[mid]>target){
+                    right=mid-1;
+                }else if(nums[mid]<target){
+                    left=mid+1;
+                }else{
+                    return mid;
+                }
+            }
+            return -1;
+        }
+    }
 }
