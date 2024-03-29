6722cb964fe4953057a811cc6a92254765f975e6
Sure <wangfengshuo2012@163.com>
2024-03-29 17:57:58 +0800

    LC34_find_first_and_last_position_of_element_in_sorted_array,Solution20240329

--- list/LC34_find_first_and_last_position_of_element_in_sorted_array.java
+++ list/LC34_find_first_and_last_position_of_element_in_sorted_array.java
@@ -60,4 +60,62 @@ public class LC34_find_first_and_last_position_of_element_in_sorted_array {
         }
         return right;
     }
+    //第二次做，竟然短时间独立做出来了，只简单调试了几次。思路也是和labuladong一致，只不过我感觉我的更高效，因为我利用了找左边界时的信息，记下了一个更小的区间来找右边界。labuladong则是单纯找左再找右
+    class Solution20240329 {
+        public int[] searchRange(int[] nums, int target) {
+            int[] result = new int[]{-1,-1};
+            if(nums==null||nums.length==0){
+                return result;
+            }
+            int left =0,right=nums.length-1;
+            int mid=0;
+            int left2=0,right2=0;
+            boolean hasFounded = false;
+            //先找左边界
+            while(left<=right){
+                mid = left+(right-left)/2;
+                if(nums[mid]>target){
+                    right=mid-1;
+                }else if(nums[mid]==target){
+                    if(!hasFounded){
+                        hasFounded=true;
+                        //如果是第一次找到，说明所有的target都在当前这个区间中，把这个区间记下来一会儿只在这个区间中找右边界！
+                        left2=left;
+                        right2=right;
+                    }
+                    right = mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }
+            if(hasFounded){
+                //找左边界时，最后left作为结果index
+                result[0]=left;
+            }
+
+            //开始找右边界
+            hasFounded=false;
+            while(left2<=right2){
+                mid = left2+(right2-left2)/2;
+                if(nums[mid]>target){
+                    right2=mid-1;
+                }else if(nums[mid]==target){
+                    if(!hasFounded){
+                        hasFounded=true;
+                    }
+                    left2 = mid+1;
+                }else{
+                    left2=mid+1;
+                }
+            }
+            if(hasFounded){
+                //找右边界时，最后right作为结果index（经验之谈）
+                result[1]=right2;
+            }
+
+            return result;
+        }
+
+
+    }
 }
