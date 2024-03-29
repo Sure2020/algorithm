085a3b14fff8e6cb141047f91294dfd5138140b2
Sure <wangfengshuo2012@163.com>
2024-03-29 23:02:46 +0800

    LC153_find_minimum_in_rotated_sorted_array,Solution20240329

--- list/LC153_find_minimum_in_rotated_sorted_array.java
+++ list/LC153_find_minimum_in_rotated_sorted_array.java
@@ -39,4 +39,37 @@ public class LC153_find_minimum_in_rotated_sorted_array {
         }
         return nums[low];
     }
+    //这是自己独立写出来的思路，虽然不如官解简洁，但仍然选择自己的思路，减少记忆成本
+    class Solution20240329 {
+        public int findMin(int[] nums) {
+            if(nums==null || nums.length==0){
+                return -50001;
+            }
+
+            int left =0,right = nums.length-1;
+            int mid = 0;
+            int min = 50001;
+            if(nums[left]<nums[right]){
+                return nums[left];
+            }
+            while(left<=right){
+                mid = left+(right-left)/2;
+                min = Math.min(min, nums[mid]);
+                // System.out.println(left + "#"+right);
+                // System.out.println("#"+nums[mid]+"#");
+                //mid在左边升序的区间，记下左区间的最小值，再去右边的无序区间去找
+                if(nums[mid]>=nums[left]){
+                    min = Math.min(min, nums[left]);
+                    left = mid+1;
+                }
+                //mid的右边有序
+                else if(nums[mid]<=nums[right]){
+                    min = Math.min(min, nums[left]);
+
+                    right = mid-1;
+                }
+            }
+            return min;
+        }
+    }
 }
