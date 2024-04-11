66792701462795132773823c48a77ca93bda141f
Sure <wangfengshuo2012@163.com>
2024-04-11 21:31:28 +0800

    LC113_path_sum_ii//不能return，应该是这里提前return了导致后面的removeLast没执行，导致case1 多一个元素4？ 是的，就是不能return，既然回溯的终止条件是root==null，那就要让每次回溯都去触发结束条件才结束，而不能在这里提前结束，先算作一个记忆点吧

--- tree/LC113_path_sum_ii.java
+++ tree/LC113_path_sum_ii.java
@@ -17,6 +17,7 @@
  */
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -48,6 +49,52 @@ public class LC113_path_sum_ii {
             findPath(root.left,targetSum-root.val);
             findPath(root.right,targetSum-root.val);
             currentPath.remove(currentPath.size()-1);
+        }
+    }
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution20240411 {
+        List<List<Integer>> result  = new ArrayList<>();
+        LinkedList<Integer> track = new LinkedList<>();
+        public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+            backtrack(root, targetSum);
+            return result;
+        }
+        public void backtrack(TreeNode root, int targetSum){
+            //回溯终止条件
+            if(root==null){
+                return;
+            }
+            track.addLast(root.val);
+            if(root.val == targetSum && root.left==null && root.right==null){
+                //track.addLast(root.val);
+                result.add(new ArrayList<Integer>(track));
+                //track.removeLast();
+                //不能return，应该是这里提前return了导致后面的removeLast没执行，导致case1 多一个元素4？
+                //是的，就是不能return，既然回溯的终止条件是root==null，那就要让每次回溯都去触发结束条件才结束，而不能在这里提前结束，先算作一个记忆点吧
+                //return;
+            }
+            //track.addLast(root.val);
+            backtrack(root.left, targetSum-root.val);
+            //track.removeLast();
+
+            //track.addLast(root.val);
+            backtrack(root.right, targetSum-root.val);
+            //track.removeLast();
+            track.removeLast();
         }
     }
 }
