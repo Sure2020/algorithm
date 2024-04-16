2ba28f41caec1443fa40bcb4c0e5cfde95b80989
Sure <wangfengshuo2012@163.com>
2024-04-16 20:01:53 +0800

    LC90_subsets_ii，Solution20240416

--- backtracking/LC90_subsets_ii.java
+++ backtracking/LC90_subsets_ii.java
@@ -58,4 +58,27 @@ public class LC90_subsets_ii {
             track.removeLast();
         }
     }
+
+    class Solution20240416 {
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<Integer> track = new LinkedList<>();
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            //先排序，让值相同的元素相邻
+            Arrays.sort(nums);
+            backtrack(0,nums);
+            return res;
+        }
+        public void backtrack(int start, int[] nums){
+            res.add(new ArrayList(track));
+            for(int i=start;i<nums.length;i++){
+                //相邻的元素值相同，不再遍历
+                if(i>start && nums[i]==nums[i-1]){
+                    continue;
+                }
+                track.add(nums[i]);
+                backtrack(i+1, nums);
+                track.removeLast();
+            }
+        }
+    }
 }
