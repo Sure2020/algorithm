f35725ac2fba287d50b2ce73a114db3ccc7925ab
Sure <wangfengshuo2012@163.com>
2024-04-25 19:35:54 +0800

    LC1115_print_foobar_alternately，用了阻塞队列

--- multithreading/LC1115_print_foobar_alternately.java
+++ multithreading/LC1115_print_foobar_alternately.java
@@ -16,6 +16,10 @@
  * --------------------------------------------------------------------
  */
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -106,6 +110,117 @@ class LC1115_print_foobar_alternately {
                 }finally {
                     lock.unlock();
                 }
+            }
+        }
+    }
+
+    //无锁超时，无法通过测试用例
+    class FooBarNoLock {
+        private int n;
+        volatile boolean fooPrinted;
+        public FooBarNoLock(int n) {
+            this.n = n;
+            fooPrinted = false;
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+            for (int i = 0; i < n;) {
+                if(!fooPrinted){
+                    // printFoo.run() outputs "foo". Do not change or remove this line.
+                    printFoo.run();
+                    i++;
+                    fooPrinted = true;
+                }
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+
+            for (int i = 0; i < n; ) {
+                if (fooPrinted){
+                    // printBar.run() outputs "bar". Do not change or remove this line.
+                    printBar.run();
+                    i++;
+                    fooPrinted=false;
+                }
+            }
+        }
+    }
+
+    class FooBarCyclicBarrier {
+        private int n;
+        private CyclicBarrier cyclicBarrier;
+        volatile Boolean finished = true;
+        public FooBarCyclicBarrier(int n) {
+            this.n = n;
+            cyclicBarrier = new CyclicBarrier(2);
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+
+            for (int i = 0; i < n; ) {
+                //finished标记，为了控制foo的打印能先执行
+                // 这里写成while(!finished);更好理解，但不知道写成if为啥也行
+                //这里误写成了!finished
+                if (finished){
+                    try {
+                        printFoo.run();
+                        finished=false;
+                        i++;
+                        cyclicBarrier.await();
+                    } catch (BrokenBarrierException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+
+            //这里i++和下面的i++重复了
+            for (int i = 0; i < n; ) {
+                try {
+                    cyclicBarrier.await();
+                }catch (Exception e){
+
+                }
+                //循环栅栏所设置的线程数都到达栅栏后，才执行bar的打印
+                printBar.run();
+                i++;
+                finished=true;
+            }
+        }
+    }
+
+    class FooBarBlockingQueue {
+        private int n;
+        private BlockingQueue fooQueue;
+        private BlockingQueue barQueue;
+        public FooBarBlockingQueue(int n) {
+            this.n = n;
+            fooQueue = new LinkedBlockingQueue<Integer>();
+            fooQueue.add(0);
+            barQueue = new LinkedBlockingQueue<Integer>();
+        }
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+
+            for (int i = 0; i < n; i++) {
+                fooQueue.take();
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                barQueue.add(0);
+            }
+        }
+
+        public void bar(Runnable printBar) throws InterruptedException {
+
+            for (int i = 0; i < n; i++) {
+                barQueue.take();
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                fooQueue.add(0);
             }
         }
     }
