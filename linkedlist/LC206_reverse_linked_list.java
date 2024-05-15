bb60526107791ad6805e6c05614f864c28832f81
Sure <wangfengshuo2012@163.com>
2024-05-15 09:58:14 +0800

    LC206_reverse_linked_list,Solution20240515

--- linkedlist/LC206_reverse_linked_list.java
+++ linkedlist/LC206_reverse_linked_list.java
@@ -115,4 +115,15 @@ public class LC206_reverse_linked_list {
             return last;
         }
     }
+    class Solution20240515 {
+        public ListNode reverseList(ListNode head) {
+            if(head==null || head.next == null){
+                return head;
+            }
+            ListNode last = reverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+            return last;
+        }
+    }
 }
