67c1dbb3d35e11c071453f93db46179f6323e4f1
Sure <wangfengshuo2012@163.com>
2024-05-20 19:58:12 +0800

    LCR140_lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof, Solution20240520，高频集训12，简单的打印倒数节点，却想复杂了，再练吧

--- linkedlist/LCR140_lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof.java
+++ linkedlist/LCR140_lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof.java
@@ -44,6 +44,12 @@ public class LCR140_lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof {
 
         return result;
     }*/
+    public static void main(String[] args) {
+        System.out.println("testing");
+        ListNode list1 = Tools.listNodeCreater(new int[]{1, 3,5});
+        Solution20240520 solution20240520 = new Solution20240520();
+        System.out.println(solution20240520.trainingPlan(list1, 2).val);
+    }
     public ListNode trainingPlan(ListNode head, int cnt) {
         if(head==null){
             return null;
@@ -63,6 +69,44 @@ public class LCR140_lian_biao_zhong_dao_shu_di_kge_jie_dian_lcof {
         }
         return slowerPoint;
 
+    }
+
+    // 单步调试了一下，事实证明相较于快慢指针，我更喜欢递归的方式
+    // 但是！双指针的方法更快！0ms，递归则要2ms
+    static class Solution20240520 {
+        int count = 0;
+        public ListNode trainingPlan(ListNode head, int cnt) {
+            //base case
+            if(head==null){
+                return head;
+            }
+            ListNode cntNode = trainingPlan(head.next, cnt);
+            count++;
+            if(count==cnt){
+                System.out.println(head.val);
+                return head;
+            }
+            //这里return cntNode，会把count==cnt返回的那个目标节点一直返回到最外层调用
+            return cntNode;
+        }
+    }
+    class Solution20240520DublePointer {
+
+        public ListNode trainingPlan(ListNode head, int cnt) {
+            if(head==null||head.next==null){
+                return head;
+            }
+            ListNode slow=head, fast=head;
+            while(cnt!=0){
+                fast=fast.next;
+                cnt--;
+            }
+            while(fast!=null){
+                slow=slow.next;
+                fast=fast.next;
+            }
+            return slow;
+        }
     }
 
 }
