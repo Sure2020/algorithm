08af8ea49e0cfe064e80905ffb5ccfcb4098abf6
Sure <wangfengshuo2012@163.com>
2024-06-11 20:26:01 +0800

    LC33_search_in_rotated_sorted_array,Solution20240611,高频集训，二分搜索，4，再练

--- binarysearch/LC33_search_in_rotated_sorted_array.java
+++ binarysearch/LC33_search_in_rotated_sorted_array.java
@@ -54,4 +54,48 @@ public class LC33_search_in_rotated_sorted_array {
             return -1;
         }
     }
+
+    //需要记住的一点是，可以通过判断mid和left对应的值的大小，判断target在断崖的哪边，然后思路就可以打开了
+//还有两处细节要注意，详见两处注意
+    class Solution20240611 {
+        public int search(int[] nums, int target) {
+            int left=0,right=nums.length-1,mid=0;
+            while(left<=right){
+                //我真是服了，又忘了计算mid
+                mid = (right-left)/2+left;
+                if(nums[mid]==target){
+                    return mid;
+                }else if(nums[mid]>=nums[left]){
+                    //注意1，mid==left的条件放在这个case中，因为情况都是mid在断崖左边
+                    //System.out.println(nums[mid] + " #### " + nums[left] + "####" + nums[right]);
+                    //mid在断崖左边，left---mid之间肯定有序，
+                    //如果target在left和mid之间，则按常规二分搜索，收缩右边界，否则收缩左边界
+                    // if(target == nums[left]){
+                    //     return left;
+                    // }
+                    //注意2
+                    if(target>=nums[left] && target<nums[mid]){
+                        right = mid-1;
+                    }else{
+                        left = mid+1;
+                    }
+                }else if(nums[mid]<nums[left]){
+                    //System.out.println(nums[mid] + " # " + nums[left] + "#" + nums[right]);
+                    //mid在断崖右边，mid---right之间肯定有序，
+                    //如果target在mid和right之间，则按常规二分搜索，收缩左边界，否则收缩右边界
+                    // if(target==nums[right]){
+                    //     return right;
+                    // }
+                    //注意2，要考虑target==边界的情况
+                    if(target>nums[mid] && target<=nums[right]){
+                        left = mid+1;
+                    }else{
+                        right = mid-1;
+                    }
+                }
+
+            }
+            return -1;
+        }
+    }
 }
