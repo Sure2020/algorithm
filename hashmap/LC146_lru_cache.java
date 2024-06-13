2298d2ae13823b9c0d91f262643ce0f4d0194805
Sure <wangfengshuo2012@163.com>
2024-06-13 20:20:45 +0800

    LC146_lru_cache,LRUCache20240613,高频集训，哈希表，2，LRU,我觉得很重要

--- hashmap/LC146_lru_cache.java
+++ hashmap/LC146_lru_cache.java
@@ -107,4 +107,50 @@ public class LC146_lru_cache {
             cache.put(key, value);
         }
     }
+
+    //浏览了一遍labuladong，才回忆起一点点，关键是借助linkedHashMap实现
+    class LRUCache20240613 {
+        private int cap;
+        private LinkedHashMap<Integer, Integer> cache = new LinkedHashMap<>();
+        public LRUCache20240613(int capacity) {
+            this.cap = capacity;
+        }
+
+        public int get(int key) {
+            if (cache.containsKey(key)){
+                makeRecent(key);
+                return cache.get(key);
+            }
+            //没有怎么办，按题意返回-1
+            return -1;
+        }
+
+        public void put(int key, int value) {
+            if(cache.containsKey(key)){
+                makeRecent(key);
+            }
+            //不存在则插入，插入前判断容量是否够，不够就删除最少使用的元素
+            //一遍遍踩坑，靠北了我真是，这里不用else而直接用if的话，如果cache已满，且上面get完了，还会走下面这个，会remove头上的元素！
+            else if(cap==cache.size()){
+                //最久未使用的元素怎么取到？
+                //todo
+                //看了labuladong知道了，链表头部元素，就是最久没使用的，也就是，链表采用的是尾插法
+                int oldestKey = cache.keySet().iterator().next();
+                cache.remove(oldestKey);
+            }
+            cache.put(key,value);
+        }
+        public  void makeRecent(int key){
+            int value = cache.get(key);
+            cache.remove(key);
+            cache.put(key,value);
+        }
+    }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
 }
