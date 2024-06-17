f6ee279091b59ec1afe8cd4203ff2498c58a89d5
Sure <wangfengshuo2012@163.com>
2024-06-17 23:14:54 +0800

    LC5_longest_palindromic_substring,Solution20240617,高频集训，动态规划，3

--- list/LC5_longest_palindromic_substring.java
+++ list/LC5_longest_palindromic_substring.java
@@ -73,4 +73,26 @@ public class LC5_longest_palindromic_substring {
         }
         return s.substring(left, right + 1);
     }
+
+    class Solution20240617 {
+        public String longestPalindrome(String s) {
+            String res = "";
+            for(int i=0;i<s.length();i++){
+                String s1 = palindrome(s,i,i);
+                String s2 = palindrome(s,i,i+1);
+                res = res.length()>s1.length()?res:s1;
+                res = res.length()>s2.length()?res:s2;
+            }
+            return res;
+        }
+        public String palindrome(String s, int l, int r){
+            while(l>=0&&r<s.length()&&s.charAt(l)==s.charAt(r)){
+                l--;
+                r++;
+            }
+            return s.substring(l+1,r);
+        }
+    }
+//没思路，直接看labuladong
+//只要记住一点，从中间向两边扩散，后面的思路就很自然了
 }
