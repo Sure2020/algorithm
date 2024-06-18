ddf6d50a09b1c8527027a5159e3b97303888ac0f
Sure <wangfengshuo2012@163.com>
2024-06-18 16:22:39 +0800

    LC88_merge_sorted_array，Solution20240618

--- list/LC88_merge_sorted_array.java
+++ list/LC88_merge_sorted_array.java
@@ -80,4 +80,35 @@ public class LC88_merge_sorted_array {
             }
         }
     }
+
+    class Solution20240618 {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int p1=m-1,p2=n-1,p3=m+n-1;
+            int n1=0,n2=0;
+            while(p1>=0&&p2>=0){
+                n1 = nums1[p1];
+                n2 = nums2[p2];
+                if(n1>=n2){
+                    nums1[p3]=n1;
+                    p1--;
+                }else{
+                    nums1[p3]=n2;
+                    p2--;
+                }
+                p3--;
+            }
+            while(p1>=0){
+                nums1[p3]=nums1[p1];
+                p1--;
+                p3--;
+            }
+            while(p2>=0){
+                nums1[p3]=nums2[p2];
+                p2--;
+                p3--;
+            }
+            // return nums1;
+        }
+    }
+//双指针没跑了,nums1从尾部插入
 }
