54f79597e6310dd69a853d27f6a39fef5b8b412d
Sure <wangfengshuo2012@163.com>
2024-06-19 16:22:45 +0800

    LC1_two_sum,Solution20240619

--- list/LC1_two_sum.java
+++ list/LC1_two_sum.java
@@ -89,4 +89,21 @@ public class LC1_two_sum {
     }
 //因为此题带哈希表标签，那就。。。将所有元素读入一个哈希表，然后依次取数组元素，在hashtable中找与target的差值
 //掉坑里了，不能一次性将所有元素都读入hashmap，要边从数组中取元素，边在Map中找元素，边向map中放元素。否则会重复取到同一个元素
+
+    class Solution20240619 {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer,Integer> map = new HashMap<>();
+            int need = 0;
+            for(int i=0;i<nums.length;i++){
+                need = target - nums[i];
+                if(map.containsKey(need)){
+                    return new int[]{i, map.get(need)};
+                }else{
+                    map.put(nums[i], i);
+                }
+            }
+            return new int[]{-1,-1};
+        }
+    }
+//这个我印象很深刻，不能一次性都放到map中，否则会将同一个元素使用两次，应该边判断边放
 }
