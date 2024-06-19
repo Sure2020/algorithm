7f630383ff1847e9e578b4714dbdcf4f8ed17d04
Sure <wangfengshuo2012@163.com>
2024-06-19 16:03:33 +0800

    LC21_merge_two_sorted_lists,Solution20240619

--- linkedlist/LC21_merge_two_sorted_lists.java
+++ linkedlist/LC21_merge_two_sorted_lists.java
@@ -187,4 +187,29 @@ public class LC21_merge_two_sorted_lists {
             return dummy.next;
         }
     }
+
+    class Solution20240619 {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            ListNode dummy = new ListNode();
+            ListNode p1=list1,p2=list2,p3=dummy;
+
+            while(p1!=null && p2!=null){
+                if(p1.val<=p2.val){
+                    p3.next = p1;
+                    p1=p1.next;
+                }else{
+                    p3.next = p2;
+                    p2 = p2.next;
+                }
+                p3 = p3.next;
+            }
+            if(p1!=null){
+                p3.next = p1;
+            }else if(p2!=null){
+                p3.next=p2;
+            }
+            return dummy.next;
+        }
+    }
+//双指针，最后再处理一下比较长的那个链表
 }
