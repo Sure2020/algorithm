e02492130e47e12c21126c8e7e017076645487a2
Sure <wangfengshuo2012@163.com>
2024-07-12 07:55:49 +0800

    LC34_find_first_and_last_position_of_element_in_sorted_array,Solution20240712

--- list/LC34_find_first_and_last_position_of_element_in_sorted_array.java
+++ list/LC34_find_first_and_last_position_of_element_in_sorted_array.java
@@ -304,4 +304,46 @@ public class LC34_find_first_and_last_position_of_element_in_sorted_array {
 //寻找左右边界的结合体，我认为相当经典
 //调完直接提交，都没先运行一下，就是这么狂，哈哈哈哈哈哈
 
+    class Solution20240712 {
+        int[] result = new int[]{-1,-1};
+        public int[] searchRange(int[] nums, int target) {
+            leftBound(nums,target);
+            rightBound(nums,target);
+            return result;
+        }
+        public void leftBound(int[] nums, int target){
+            int left = 0,right = nums.length-1,mid=0;
+            boolean founded = false;
+            while(left<=right){
+                mid = left+(right-left)/2;
+                if(nums[mid]==target){
+                    founded = true;
+                    right = mid-1;
+                }else if(nums[mid]>target){
+                    right = mid-1;
+                }else {
+                    left = mid+1;
+                }
+            }
+            result[0] = founded?left:-1;
+        }
+        public void rightBound(int[] nums, int target){
+            int left=0,right=nums.length-1,mid=0;
+            boolean founded = false;
+            while(left<=right){
+                mid = left+(right-left)/2;
+                if(nums[mid]==target){
+                    founded = true;
+                    left=mid+1;
+                }else if(nums[mid]>target){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            result[1] = founded?right:-1;
+        }
+    }
+//寻找左右边界的结合体
+
 }
