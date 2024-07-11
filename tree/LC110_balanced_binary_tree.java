11ef676654ab225e2dc5173a8175a438afafd218
Sure <wangfengshuo2012@163.com>
2024-07-11 08:19:58 +0800

    LC110_balanced_binary_tree,Solution20240711

--- tree/LC110_balanced_binary_tree.java
+++ tree/LC110_balanced_binary_tree.java
@@ -198,4 +198,29 @@ public class LC110_balanced_binary_tree {
             return depth;
         }
     }
+
+    class Solution20240711 {
+        boolean result = true;
+        public boolean isBalanced(TreeNode root) {
+            if(root==null){
+                return true;
+            }
+            height(root);
+            return result;
+        }
+
+        public int height(TreeNode root){
+            if(root==null){
+                return 0;
+            }
+            int leftHeight = height(root.left);
+            int rightHeight = height(root.right);
+            if(Math.abs(leftHeight-rightHeight)>1){
+                result = false;
+                return -1;
+            }
+            return Math.max(leftHeight,rightHeight)+1;
+        }
+    }
+//分解的思路
 }
