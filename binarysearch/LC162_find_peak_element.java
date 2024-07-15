c75b20a5553afe4f869496d593ac8ebcfdfdeaea
Sure <wangfengshuo2012@163.com>
2024-07-15 21:08:23 +0800

    LC162_find_peak_element,Solution20240715

--- binarysearch/LC162_find_peak_element.java
+++ binarysearch/LC162_find_peak_element.java
@@ -189,4 +189,33 @@ public class LC162_find_peak_element {
         }
     }
 //keys:通过比较mid和mid+1，确定峰值在mid的哪边
+
+    class Solution20240715 {
+        public int findPeakElement(int[] nums) {
+            // if(nums.length==0){
+            //     return -1;
+            // }
+            // if(nums.length==1){
+            //     return 0;
+            // }
+            int left=0,right=nums.length-1,mid=0;
+            while(left<=right){
+                mid = left+(right-left)/2;
+                if(mid+1>=nums.length){
+                    return mid;
+                }
+                //峰值是mid或在mid左边
+                if(nums[mid]>nums[mid+1]){
+                    right = mid-1;
+                }
+                //峰值在mid右边
+                else if(nums[mid]<nums[mid+1]){
+                    left = mid+1;
+                }
+            }
+            return left;
+        }
+    }
+//二分查找，比较mid和mid+1的大小，确定峰值在mid左边还是右边
+//写出框架，然后面向测试用例编程，修补边缘情况
 }
