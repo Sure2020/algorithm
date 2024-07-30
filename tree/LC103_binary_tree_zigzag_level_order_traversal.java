8d05d1c676069144f75f0b6a2a9e1673693a4245
Sure <wangfengshuo2012@163.com>
2024-07-31 07:59:21 +0800

    LC103_binary_tree_zigzag_level_order_traversal，Solution20240731

--- tree/LC103_binary_tree_zigzag_level_order_traversal.java
+++ tree/LC103_binary_tree_zigzag_level_order_traversal.java
@@ -294,4 +294,40 @@ public class LC103_binary_tree_zigzag_level_order_traversal {
     }
 
 //我直到要用尾插法，但好像没想到用linkedlist支持尾插法，瞟到了之前的思路而已。。。
+
+    class Solution20240731 {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if(root==null){
+                return result;
+            }
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            boolean leftToRight = true;
+            while(!queue.isEmpty()){
+                int size = queue.size();
+                LinkedList<Integer> tempList = new LinkedList<>();
+                for(int i=1;i<=size;i++){
+                    TreeNode tempNode = queue.poll();
+                    if(leftToRight){
+                        tempList.addLast(tempNode.val);
+                    }else{
+                        tempList.addFirst(tempNode.val);
+                    }
+                    if(tempNode.left!=null){
+                        queue.offer(tempNode.left);
+                    }
+                    if(tempNode.right!=null){
+                        queue.offer(tempNode.right);
+                    }
+                }
+                result.add(tempList);
+
+                leftToRight = !leftToRight;
+            }
+            return result;
+        }
+    }
+
+//这个题记个星星吧，因为瞟到了之前的思路，linkedlist支持头插和尾插
 }
