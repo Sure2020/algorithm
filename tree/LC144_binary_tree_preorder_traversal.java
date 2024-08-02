206bbbc26ba34693a73ab991e733d17254753e75
Sure <wangfengshuo2012@163.com>
2024-08-02 08:03:59 +0800

    LC144_binary_tree_preorder_traversal,Solution20240802

--- tree/LC144_binary_tree_preorder_traversal.java
+++ tree/LC144_binary_tree_preorder_traversal.java
@@ -114,4 +114,18 @@ public class LC144_binary_tree_preorder_traversal {
             return result;
         }
     }
+
+    class Solution20240802 {
+        List<Integer> result = new ArrayList<>();
+        public List<Integer> preorderTraversal(TreeNode root) {
+
+            if(root==null){
+                return result;
+            }
+            result.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+            return result;
+        }
+    }
 }
