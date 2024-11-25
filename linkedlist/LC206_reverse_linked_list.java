9a51e1c452ba8246718a4b0237da501c81014f96
Sure <wangfengshuo2012@163.com>
2024-11-25 08:25:00 +0800

    LC206_reverse_linked_list,Solution20241125

--- linkedlist/LC206_reverse_linked_list.java
+++ linkedlist/LC206_reverse_linked_list.java
@@ -225,4 +225,20 @@ public class LC206_reverse_linked_list {
             return previous;
         }
     }
+
+    class Solution20241125 {
+        public ListNode reverseList(ListNode head) {
+            if(head==null|| head.next==null){
+                return head;
+            }
+            ListNode pre = null,cur = head,next=null;
+            while(cur!=null){
+                next=cur.next;
+                cur.next = pre;
+                pre = cur;
+                cur = next;
+            }
+            return pre;
+        }
+    }
 }
