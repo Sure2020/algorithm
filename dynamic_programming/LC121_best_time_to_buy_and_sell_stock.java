44e5c0cb445f6d4cfee1da97ed02a2ce7eba6149
Sure <wangfengshuo2012@163.com>
2024-11-27 07:31:58 +0800

    LC121_best_time_to_buy_and_sell_stock,Solution20241127

--- dynamic_programming/LC121_best_time_to_buy_and_sell_stock.java
+++ dynamic_programming/LC121_best_time_to_buy_and_sell_stock.java
@@ -25,4 +25,20 @@ public class LC121_best_time_to_buy_and_sell_stock {
     }
 //我能想到的是，1，穷举；2，将value与index的映射写入map，再排序数组，找数组两头的元素（不行，因为有先后顺序。。。）
 //看了Krahets的题解
+
+    class Solution20241127 {
+        public int maxProfit(int[] prices) {
+            int length = prices.length;
+            if(length<2){
+                return 0;
+            }
+            int cost=Integer.MAX_VALUE,result = 0;
+            for(int price: prices){
+                cost = Math.min(cost, price);
+                result = Math.max(result, price-cost);
+            }
+            return result;
+        }
+    }
+//由于for循环是不断向前推进的，所以这里不会出现先卖后买的情况。
 }
