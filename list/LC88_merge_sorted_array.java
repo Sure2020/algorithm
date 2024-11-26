364918f478b33e988482d01bef123ed99304f482
Sure <wangfengshuo2012@163.com>
2024-11-26 09:13:19 +0800

    LC88_merge_sorted_array,Solution20241126

--- list/LC88_merge_sorted_array.java
+++ list/LC88_merge_sorted_array.java
@@ -202,4 +202,25 @@ public class LC88_merge_sorted_array {
     }
 //从后往前塞元素
 //教训就是，是在原基础上继续补救，还是重写新思路
+    class Solution20241126 {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int index1 = m-1,index2=n-1,index3=m+n-1;
+            while(index1>=0&&index2>=0){
+                if(nums1[index1]>=nums2[index2]){
+                    nums1[index3]=nums1[index1];
+                    index1--;
+                }else{
+                    nums1[index3]=nums2[index2];
+                    index2--;
+                }
+                index3--;
+            }
+            while(index2>=0 && index3>=0){
+                nums1[index3]=nums2[index2];
+                index3--;
+                index2--;
+            }
+            //return nums1;
+        }
+    }
 }
