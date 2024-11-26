86a27ab7d4c2062bcbd03ca9a4012320893bd601
Sure <wangfengshuo2012@163.com>
2024-11-26 23:24:47 +0800

    LC1_two_sum,Solution20241126

--- list/LC1_two_sum.java
+++ list/LC1_two_sum.java
@@ -143,4 +143,19 @@ public class LC1_two_sum {
         }
     }
 //根据当前的值，求需要的值，并去map中找，找不到就把当前值放入map，继续判断下一个元素。
+
+    class Solution20241126 {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int i=0;i<nums.length;i++){
+                int need = target-nums[i];
+                if(map.containsKey(need)){
+                    return new int[]{i,map.get(need)};
+                }else{
+                    map.put(nums[i],i);
+                }
+            }
+            return new int[]{-1,-1};
+        }
+    }
 }
