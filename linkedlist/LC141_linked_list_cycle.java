605d40d82ebbd5d245f39f69634e8ec6de81d52a
Sure <wangfengshuo2012@163.com>
2024-11-27 20:06:29 +0800

    LC141_linked_list_cycle,Solution20241127

--- linkedlist/LC141_linked_list_cycle.java
+++ linkedlist/LC141_linked_list_cycle.java
@@ -148,4 +148,21 @@ public class LC141_linked_list_cycle {
             return false;
         }
     }
+    public class Solution20241127 {
+        public boolean hasCycle(ListNode head) {
+            if(head==null || head.next==null){
+                return false;
+            }
+            ListNode p1 = head,p2=head;
+            while(p1!=null && p2!=null && p2.next!=null){
+                p1=p1.next;
+                p2=p2.next.next;
+                if(p1==p2){
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+//快慢指针，快走二，慢走一。
 }
