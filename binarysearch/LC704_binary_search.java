77ae3fc414cbadb440593324c20f4611e6209154
Sure <wangfengshuo2012@163.com>
2024-11-27 22:37:51 +0800

    LC704_binary_search,Solution20241127

--- binarysearch/LC704_binary_search.java
+++ binarysearch/LC704_binary_search.java
@@ -174,4 +174,23 @@ public class LC704_binary_search {
         }
     }
 //事后：有点惊险，原来是弄错了><号的方向
+
+    class Solution20241127 {
+        public int search(int[] nums, int target) {
+            int left=0,right=nums.length-1,mid=0;
+            while(left<=right){
+                mid=left+(right-left)/2;
+                if(nums[mid]==target){
+                    return mid;
+                }
+                else if(nums[mid]>target){
+                    right = mid-1;
+                }else{
+                    left = mid+1;
+                }
+            }
+            return -1;
+        }
+    }
+//从中间开始，二分查找
 }
