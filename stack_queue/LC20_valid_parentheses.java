7c205b4b0b0f61975afc913e6f30a9909e1fb656
Sure <wangfengshuo2012@163.com>
2024-11-27 08:11:41 +0800

    LC20_valid_parentheses,Solution20241127

--- stack_queue/LC20_valid_parentheses.java
+++ stack_queue/LC20_valid_parentheses.java
@@ -65,4 +65,57 @@ public class LC20_valid_parentheses {
             }
         }
     }
+
+    class Solution20241127 {
+        public boolean isValid(String s) {
+            int length = s.length();
+            if(length<=1){
+                return false;
+            }
+            Stack<Character> stack = new Stack<>();
+            for(int i=0;i<length;i++){
+                char c = s.charAt(i);
+                //System.out.println("current char: " + c);
+                //遇到左括号，入栈
+                //这里挺遗憾的，错把{写成了}，调试了一会儿。细心很重要哇，写的时候不要急。
+                if(c=='(' || c=='[' || c=='{'){
+                    stack.push(c);
+                }
+                //遇到右括号，开始判断是否有匹配的左括号。
+                else{
+                    // if(!stack.isEmpty()){
+                    //     System.out.println("left of: " + leftOf(c));
+                    //     System.out.println("peek: " + stack.peek());
+                    // }
+
+                    if(!stack.isEmpty() && leftOf(c)==stack.peek()){
+                        //匹配上了，就弹出，证明这一组括号没问题，以后不用关注
+                        stack.pop();
+                    }else{
+                        // if(stack.isEmpty()){
+                        //     System.out.println("stack is empty");
+                        //     return false;
+                        // }
+                        //System.out.println("left of: " + leftOf(c));
+                        //System.out.println("peek: " + stack.peek());
+                        //没匹配上，直接返回false
+                        return false;
+                    }
+                }
+            }
+            return stack.isEmpty();
+        }
+        public char leftOf(char c){
+            if(c==')'){
+                return '(';
+            }else if(c==']'){
+                return '[';
+            }else if(c=='}'){
+                return '{';
+            }else {
+                return ' ';
+            }
+        }
+    }
+//利用栈，先将左括号存起来，当遇到右括号的时候，开始从栈中寻找匹配的左括号
 }
