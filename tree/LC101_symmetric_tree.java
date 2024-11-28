bd217489a82e32f746861778ba529d15e5f6a886
Sure <wangfengshuo2012@163.com>
2024-11-28 19:49:14 +0800

    LC101_symmetric_tree,Solution20241128

--- tree/LC101_symmetric_tree.java
+++ tree/LC101_symmetric_tree.java
@@ -254,4 +254,26 @@ class LC101_symmetric_tree {
     }
 //反正不能用层序遍历，要用迭代，左子树和右子树对比，右子树和左子树对比
 //事后：不能对比==就随便返回true，因为可能当前为true，后面为false。应该遇到false时返回false！
+
+    class Solution20241128 {
+        public boolean isSymmetric(TreeNode root) {
+            if(root==null){
+                return false;
+            }
+            return compare(root,root);
+        }
+        public boolean compare(TreeNode node1, TreeNode node2){
+            if(node1==null && node2==null){
+                return true;
+            }
+            if(node1==null || node2==null){
+                return false;
+            }
+            if(node1.val != node2.val){
+                return false;
+            }
+            return compare(node1.left, node2.right) && compare(node1.right, node2.left);
+        }
+    }
+//递归，我大概回忆起了一半以上，搞个函数，入参是两个节点，函数内对比并递归对比入参1的左节点和入参2的右节点，入参2的右节点和入参1的左节点。
 }
