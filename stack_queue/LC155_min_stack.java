b216ff44f163276ea80c2e9fc94f61b191535fa1
Sure <wangfengshuo2012@163.com>
2024-11-28 09:47:47 +0800

    LC155_min_stack,MinStack20241128

--- stack_queue/LC155_min_stack.java
+++ stack_queue/LC155_min_stack.java
@@ -58,4 +58,36 @@ public class LC155_min_stack {
             return minStk.peek();
         }
     }
+
+    class MinStack20241128 {
+        private Stack<Integer> stack;
+        private Stack<Integer> minStack;
+        public MinStack20241128() {
+            stack = new Stack<>();
+            minStack = new Stack<>();
+        }
+
+        public void push(int val) {
+            if(minStack.isEmpty() || val<=minStack.peek()){
+                minStack.push(val);
+            }else{
+                minStack.push(minStack.peek());
+            }
+            stack.push(val);
+        }
+
+        public void pop() {
+            minStack.pop();
+            stack.pop();
+        }
+
+        public int top() {
+            return stack.peek();
+        }
+
+        public int getMin() {
+            return minStack.peek();
+        }
+    }
+//关键是搞个stack专门存当前元素入栈后的最小值
 }
