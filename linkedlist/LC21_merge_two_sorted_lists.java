48ecb9da5aef2b61aa59ff3ef58643c46862e194
Sure <wangfengshuo2012@163.com>
2025-05-13 14:07:35 +0800

    LC21_merge_two_sorted_lists,Solution20250513

--- linkedlist/LC21_merge_two_sorted_lists.java
+++ linkedlist/LC21_merge_two_sorted_lists.java
@@ -298,4 +298,34 @@ public class LC21_merge_two_sorted_lists {
             return dummy.next;
         }
     }
+
+    class Solution20250513 {
+        public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+            if(list1==null){
+                return list2;
+            }
+            if(list2==null){
+                return list1;
+            }
+            ListNode dummy = new ListNode();
+            ListNode p1=list1,p2=list2,p3=dummy;
+            while(p1!=null && p2!=null){
+                if(p1.val<=p2.val){
+                    p3.next=p1;
+                    p1=p1.next;
+                }else{
+                    p3.next=p2;
+                    p2=p2.next;
+                }
+                p3=p3.next;
+            }
+            if(p1!=null){
+                p3.next=p1;
+            }
+            if(p2!=null){
+                p3.next=p2;
+            }
+            return dummy.next;
+        }
+    }
 }
