d377519de8b8f65c282167254897e2d4e8e9e0c2
Sure <wangfengshuo2012@163.com>
2025-05-17 13:32:18 +0800

    LC3_longest_substring_without_repeating_characters,Solution20250517

--- slidingwindow/LC3_longest_substring_without_repeating_characters.java
+++ slidingwindow/LC3_longest_substring_without_repeating_characters.java
@@ -284,4 +284,28 @@ public class LC3_longest_substring_without_repeating_characters {
         }
     }
 //滑动窗口
+
+    class Solution20250517 {
+        public int lengthOfLongestSubstring(String s) {
+            if(s==null || s.length()==0){
+                return 0;
+            }
+            Set<Character> set = new HashSet<>();
+            int left=0,right=0,result=0;
+            while(right<s.length()){
+                char c = s.charAt(right);
+                if(!set.contains(c)){
+                    set.add(c);
+                    result = Math.max(result, set.size());
+                    right++;
+                }else{
+                    char d = s.charAt(left);
+                    set.remove(d);
+                    left++;
+                }
+
+            }
+            return result;
+        }
+    }
 }
