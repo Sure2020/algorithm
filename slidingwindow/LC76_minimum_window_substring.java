f47402f7d1e8d0f45a13038a42c97e313049ec4c
Sure <wangfengshuo2012@163.com>
2025-05-19 18:05:21 +0800

    LC76_minimum_window_substring,Solution20250519

--- slidingwindow/LC76_minimum_window_substring.java
+++ slidingwindow/LC76_minimum_window_substring.java
@@ -555,4 +555,49 @@ public class LC76_minimum_window_substring {
             return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
         }
     }
+
+    class Solution20250519 {
+        public String minWindow(String s, String t) {
+            Map<Character, Integer> need = new HashMap<>();
+            Map<Character, Integer> window = new HashMap<>();
+
+            for(char c:t.toCharArray()){
+                need.put(c,need.getOrDefault(c,0)+1);
+            }
+
+            int left = 0, right=0;
+            int valid = 0;
+            int start = 0, minLen = Integer.MAX_VALUE;
+            while(right<s.length()){
+                char c = s.charAt(right);
+                right ++;
+                if(need.containsKey(c)){
+                    window.put(c, window.getOrDefault(c,0)+1);
+                    if(window.get(c).equals(need.get(c))){
+                        valid ++;
+                    }
+                }
+
+                while(valid == need.size()){
+                    //这里竟然写成了right - left> minLen。。。
+                    if(right - left < minLen){
+                        start = left;
+                        minLen = right-left;
+                    }
+                    char d = s.charAt(left);
+                    left++;
+                    if(need.containsKey(d)){
+                        if(  window.get(d).equals(need.get(d))  ){
+                            valid --;
+                        }
+                        window.put(d, window.get(d) -1);
+                    }
+
+                }
+
+
+            }
+            return minLen == Integer.MAX_VALUE?"":s.substring(start, start+minLen);
+        }
+    }
 }
