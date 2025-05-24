a0a11e3247ccb77f8706979ab4c7197b644ccbde
Sure <wangfengshuo2012@163.com>
2025-05-25 00:13:36 +0800

    LC88_merge_sorted_array,Solution20250525

--- list/LC88_merge_sorted_array.java
+++ list/LC88_merge_sorted_array.java
@@ -265,4 +265,29 @@ public class LC88_merge_sorted_array {
             }
         }
     }
+    class Solution20250525 {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int index1 = m-1,index2=n-1,index3=m+n-1;
+            while(index1>=0 && index2>=0){
+                if(nums1[index1]>=nums2[index2]){
+                    nums1[index3]=nums1[index1];
+                    index1--;
+                }else{
+                    nums1[index3]=nums2[index2];
+                    index2--;
+                }
+                index3--;
+            }
+            while(index1>=0){
+                nums1[index3]=nums1[index1];
+                index1--;
+                index3--;
+            }
+            while(index2>=0){
+                nums1[index3]=nums2[index2];
+                index2--;
+                index3--;
+            }
+        }
+    }
 }
