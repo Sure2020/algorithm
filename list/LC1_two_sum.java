60e4c44bc9c4865bd98e3f60e625fdd5e3023420
Sure <wangfengshuo2012@163.com>
2025-05-24 23:42:43 +0800

    LC1_two_sum,Solution20250524

--- list/LC1_two_sum.java
+++ list/LC1_two_sum.java
@@ -173,4 +173,18 @@ public class LC1_two_sum {
             return result;
         }
     }
+    class Solution20250524 {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int i=0;i<nums.length;i++){
+                int need = target-nums[i];
+                if(map.containsKey(need)){
+                    return new int[]{i, map.get(need)};
+                }else{
+                    map.put(nums[i], i);
+                }
+            }
+            return new int[]{-1,-1};
+        }
+    }
 }
