68097bd78111e0dd9a7a4d7bd3f0d85faa0385e9
Sure <wangfengshuo2012@163.com>
2025-05-25 15:41:03 +0800

    LC70_climbing_stairs,Solution20250525

--- dynamic_programming/LC70_climbing_stairs.java
+++ dynamic_programming/LC70_climbing_stairs.java
@@ -79,4 +79,18 @@ class Solution20241128 {
             return n3;
         }
     }
+    class Solution20250525 {
+        public int climbStairs(int n) {
+            if(n==0 || n==1 || n==2){
+                return n;
+            }
+            int n1=1,n2=2,n3=0;
+            for(int i=3;i<=n;i++){
+                n3=n1+n2;
+                n1=n2;
+                n2=n3;
+            }
+            return n3;
+        }
+    }
 }
