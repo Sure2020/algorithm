26933d1b403494fc8891ffee94549fb010dd0612
Sure <wangfengshuo2012@163.com>
2025-05-25 12:35:26 +0800

    LC704_binary_search,Solution20250525

--- binarysearch/LC704_binary_search.java
+++ binarysearch/LC704_binary_search.java
@@ -211,4 +211,22 @@ public class LC704_binary_search {
             return -1;
         }
     }
+
+    class Solution20250525 {
+        public int search(int[] nums, int target) {
+            int left=0,right=nums.length-1,mid=0;
+            while(left<=right){
+                mid = left+(right-left)/2;
+                if(nums[mid]==target){
+                    return mid;
+                }
+                if(nums[mid]>=target){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }
+            return -1;
+        }
+    }
 }
