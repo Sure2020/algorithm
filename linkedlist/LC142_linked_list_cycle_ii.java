cf41f464939969ab05bb275e372e9d029fe78a9a
Sure <wangfengshuo2012@163.com>
2025-05-25 12:12:57 +0800

    LC142_linked_list_cycle_ii,Solution20250525

--- linkedlist/LC142_linked_list_cycle_ii.java
+++ linkedlist/LC142_linked_list_cycle_ii.java
@@ -298,4 +298,29 @@ public class LC142_linked_list_cycle_ii {
             return slow;
         }
     }
+
+    public class Solution20250525 {
+        public ListNode detectCycle(ListNode head) {
+            if(head==null ){
+                return null;
+            }
+            ListNode slow=head,fast=head;
+            while(fast!=null&&fast.next!=null){
+                fast=fast.next.next;
+                slow=slow.next;
+                if(fast==slow){
+                    break;
+                }
+            }
+            if(fast==null || fast.next==null){
+                return null;
+            }
+            slow=head;
+            while(slow!=fast){
+                slow=slow.next;
+                fast=fast.next;
+            }
+            return slow;
+        }
+    }
 }
